161812c11f96b7182c81ba81c883f71dd428801c
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-12-06 16:12:30 +0400

    add second

--- main.js
+++ main.js
@@ -18,3 +18,52 @@ console.log(showResult(1, 4, 9, 10, 45, 289))
 console.log(showResult(7, 2, 139))
 
 // მეორე დავალება 
+const user = {
+    fName: "Marta",
+    lName: "Smith",
+    age: 45,
+    banks: [
+        {
+            bName: "TBC",
+            address: {
+                city: "Tbilisi",
+                Country: "Georgia"
+            }
+
+        },
+        {
+            bName: "Liberty",
+            address: {
+                city: "Tbilisi",
+                Country: "Georgia"
+            }
+        },
+        {
+            bName: "BOG",
+            address: {
+                city: "Tbilisi",
+                Country: "Georgia"
+            }
+
+        },
+        {
+            bName: "The Bank of London",
+            address: {
+                city: "London",
+                Country: "UK"
+            }
+        },
+        {
+            bName: "New York Community Bank",
+            address: {
+                city: "New York",
+                Country: "USA"
+            }
+
+        }         
+    ]
+}
+const {fName, lName, age, banks:[, , {bName, address:{city={}, Country}}]} = user;
+console.log(city);
+
+// მესამე დავალება
